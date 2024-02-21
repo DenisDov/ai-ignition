66855577c252c6596e7689dc164ada2c15814237
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-21 18:53:34 +0200

    connect vercel postgres

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  const { rows } = await sql`SELECT * from PLAYERS`;
+  console.log("rows: ", rows);
   return <div className="flex items-center justify-between">test</div>;
 }
