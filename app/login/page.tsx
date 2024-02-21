f47d2cbb937094b92768e4aa403ce2b884172b29
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-21 21:56:08 +0200

    fixes auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,16 @@
+import { signIn } from "@/auth";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button>login with google</button>
+      </form>
+    </div>
+  );
 }
--- auth.ts
+++ auth.ts
@@ -1,12 +1,15 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
-import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
+import { authConfig } from "./auth.config";
 
-export const { auth, signIn, signOut } = NextAuth({
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth({
   ...authConfig,
   providers: [
-    Credentials({}),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
