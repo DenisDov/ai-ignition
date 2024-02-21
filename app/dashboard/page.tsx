08d5171bf6916592d6b16cadb88a2f76a1fad531
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-21 21:10:25 +0200

    add dummy pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="space-y-2">
+      <h1 className="text-3xl font-bold">DashboardPage</h1>
+      <p>This page is protected</p>
+    </div>
+  );
 }
