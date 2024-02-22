ecc43169cb7baec5b59c8752484878395376066c
Denys Dovzhenko <dovzdenis@gmail.com>
2024-02-22 23:27:32 +0200

    move signout to header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,30 +1,6 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
 import { sql } from "@vercel/postgres";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { getInitials } from "@/lib/utils";
-
-function SignOut({ children }: { children: React.ReactNode }) {
-  return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
-      }}
-    >
-      {children}
-      <div>
-        <Button type="submit">Sign Out</Button>
-      </div>
-    </form>
-  );
-}
 
 export default async function DashboardPage() {
-  let session = await auth();
-  let user = session?.user;
-  console.log("user: ", user);
-
   const { rows } = await sql`SELECT * from USERS`;
   console.log("rows: ", rows);
 
@@ -32,19 +8,6 @@ export default async function DashboardPage() {
     <div className="space-y-2">
       <h1 className="text-3xl font-bold">DashboardPage</h1>
       <p>This page is protected</p>
-
-      {user && (
-        <div>
-          <SignOut>
-            <p>{`Welcome ${user?.name || user.email}`}</p>
-            <p>{user?.email}</p>
-            <Avatar>
-              <AvatarImage src={user?.image!} />
-              <AvatarFallback>{getInitials(user?.name! || "O")}</AvatarFallback>
-            </Avatar>
-          </SignOut>
-        </div>
-      )}
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,43 @@
+import { auth, signOut } from "@/auth";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getInitials } from "@/lib/utils";
 
-const Header = () => {
+function SignOut({ children }: { children: React.ReactNode }) {
+  return (
+    <form
+      className="flex items-center gap-2 absolute right-4"
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      {children}
+      <div>
+        <Button type="submit">Sign Out</Button>
+      </div>
+    </form>
+  );
+}
+
+const Header = async () => {
+  let session = await auth();
+  let user = session?.user;
   return (
     <div className="h-20 flex justify-center items-center bg-white">
       <Link href="/" className="text-3xl">
         AI Ignition
       </Link>
+      {user && (
+        <SignOut>
+          <p>{user?.name || user.email}</p>
+          <Avatar>
+            <AvatarImage src={user?.image!} />
+            <AvatarFallback>{getInitials(user?.name! || "O")}</AvatarFallback>
+          </Avatar>
+        </SignOut>
+      )}
     </div>
   );
 };
